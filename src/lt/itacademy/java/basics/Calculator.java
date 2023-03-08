e0c7737eb0ec4a601e145954e6aec4eea8760132
Evelina <venckute.evelina1@gmail.com>
2023-03-08 11:32:59 +0200

    Task Calculator

--- src/lt/itacademy/java/basics/Calculator.java
+++ src/lt/itacademy/java/basics/Calculator.java
@@ -1,4 +1,5 @@
 package lt.itacademy.java.basics;
+import java.util.Scanner;
 
 public class Calculator {
     //Using what you have learned create a calculator which will calculate figures perimeter and area.
@@ -7,7 +8,25 @@ public class Calculator {
     // if not write an error message that values were incorrect. Calculator must be able to calculate Triangle, Rectangle and Square perimeter
     // and area.
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Choose a figure from mentioned : Triangle, Rectangle and Square : ");
+            myString = input.next();
+            switch (myString) {
+                case "Triangle":
+                    System.out.println("Triangle!");
+                    break;
+                case "Rectangle":
+                    System.out.println("Rectangle!");
+                    break;
+                case "Square":
+                    System.out.println("Square!");
+                    break;
+                default:
+                    System.out.println("Invalid figure. Please choose again.");
+                    break;
+            }
+        } while (!myString.equals("Stop"));
     }
 
 }
