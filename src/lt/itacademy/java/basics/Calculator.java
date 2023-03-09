c8bbf7cc79b119267417a8007fe920d8585b99dc
eveven7 <venckute.evelina1@gmail.com>
2023-03-09 08:40:04 +0200

    Calculator task

--- src/lt/itacademy/java/basics/Calculator.java
+++ src/lt/itacademy/java/basics/Calculator.java
@@ -1,32 +1,82 @@
+/**
+ * Using what you have learned create a calculator which will calculate figures perimeter and area.
+ * you must use switch statement for choosing a figure. Program should run as long as you want to continue using calculator.
+ * This means that after calculations are done it must ask if you want to run it again. You must check whether input values are correct
+ * if not write an error message that values were incorrect. Calculator must be able to calculate Triangle, Rectangle and Square perimeter
+ * and area.
+ **/
 package lt.itacademy.java.basics;
+
 import java.util.Scanner;
+import static java.lang.Math.sqrt;
 
 public class Calculator {
-    //Using what you have learned create a calculator which will calculate figures perimeter and area.
-    // You must use switch statement for choosing a figure. Program should run as long as you want to continue using calculator.
-    // This means that after calculations are done it must ask if you want to run it again. You must check whether input values are correct
-    // if not write an error message that values were incorrect. Calculator must be able to calculate Triangle, Rectangle and Square perimeter
-    // and area.
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        do {
-            System.out.println("Choose a figure from mentioned : Triangle, Rectangle and Square : ");
-            myString = input.next();
-            switch (myString) {
-                case "Triangle":
-                    System.out.println("Triangle!");
+
+    public static void task10(){
+
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please input the value(three options: rectangle, triangle and square ");
+            String shape = scanner.nextLine();
+
+            switch (shape){
+                case "rectangle":
+                    //code for rectangle perimeter and area
+                    Scanner scanner2 = new Scanner(System.in);
+                    System.out.println("Please input the length value ");
+                    int length = scanner2.nextInt();
+                    System.out.println("Please input the width value ");
+                    int width = scanner2.nextInt();
+
+                    if(length ==0 || width ==0){
+                        System.out.println("Please don't insert an zeros ");
+                    }else {
+                        int perimeter = (length + width) * 2;
+                        int area = length * width;
+                        System.out.println("Rectangle perimeter is " + perimeter + " and area is " + area );
+
+                    }
+
                     break;
-                case "Rectangle":
-                    System.out.println("Rectangle!");
+                case "square":
+                    //code for square perimeter and area
+                    Scanner scanner3 = new Scanner(System.in);
+                    System.out.println("Please input the side value ");
+                    int side = scanner3.nextInt();
+
+                    if(side ==0){
+                        System.out.println("Please don't insert an zeros ");
+                    }else {
+                        int perimeter = side * 4;
+                        int area = side * side;
+                        System.out.println("Square perimeter is " + perimeter + " and area is " + area );
+
+                    }
+
                     break;
-                case "Square":
-                    System.out.println("Square!");
+                case "triangle":
+                    //code for triangle perimeter and area
+                    Scanner scanner4 = new Scanner(System.in);
+                    System.out.println("Please input the base value ");
+                    float base = scanner4.nextFloat();
+                    System.out.println("Please input the height value ");
+                    float height = scanner4.nextFloat();
+
+                    if(base ==0 || height ==0){
+                        System.out.println("Please don't insert an zeros ");
+                    }else {
+                        float hypotenusSquared = (base * base) + (height * height);
+                        float hypotenus = (float) sqrt(hypotenusSquared);
+                        float perimeter = (base + height) + hypotenus;
+                        float area = (base * 0.5f) * height;
+                        System.out.println("Rectangle perimeter is " + perimeter + " and area is " + area );
+
+                    }
                     break;
                 default:
-                    System.out.println("Invalid figure. Please choose again.");
-                    break;
+                    System.out.println("Can't recognise the shape, please insert a valid shape option");
             }
-        } while (!myString.equals("Stop"));
-    }
+        }
 
+    }
 }
